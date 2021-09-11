e9389237546df79962effbba69bcf91013902e33
Gibran Tavares <gibran.tavares@fortbrasil.com.br>
2021-09-11 15:20:06 -0300

    feat: tuples

--- app.ts
+++ app.ts
@@ -1,1 +1,13 @@
-console.log('Your code goes here...');
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
+    name: 'Gibran',
+    age: 29,
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
+}
+
+person.role = [2, '']
