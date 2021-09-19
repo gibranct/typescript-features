8abea699e8f247cb1d3160e90535c5cf669ccd83
Gibran Tavares <gibran.tavares@fortbrasil.com.br>
2021-09-19 10:51:34 -0300

    feat: add form inside div#app

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,43 @@
-// Code goes here!
+class ProjectInput {
+    templateFormEl: HTMLTemplateElement
+    appContainer: HTMLDivElement
+    formElement: HTMLFormElement
+    titleInput: HTMLInputElement
+    descriptionTextArea: HTMLTextAreaElement
+    numberInput: HTMLInputElement
+
+
+    constructor() {
+        this.templateFormEl = document.getElementById('project-input')! as HTMLTemplateElement
+        this.appContainer = document.getElementById('app')! as HTMLDivElement
+
+        const importedNode = document.importNode(this.templateFormEl.content, true)
+        this.formElement = importedNode.firstElementChild as HTMLFormElement 
+        this.formElement.id = 'user-input'
+
+        this.titleInput = this.formElement.querySelector('#title')!
+        this.descriptionTextArea = this.formElement.querySelector('#description')!
+        this.numberInput = this.formElement.querySelector('#people')!
+
+
+        this.configure()
+        this.attach()
+    }
+
+    private configure() {
+        this.formElement.addEventListener('submit', this.submitHandler)
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault()
+        console.log(this.titleInput.value);
+        
+    }
+
+    private attach() {
+        console.log(this.formElement);
+        this.appContainer.insertAdjacentElement('afterbegin', this.formElement)
+    }
+}
+
+const pInput = new ProjectInput()
