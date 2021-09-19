29c49a522ae24ee36105f20458cbc057d6182864
Gibran Tavares <gibran.tavares@fortbrasil.com.br>
2021-09-19 16:19:25 -0300

    feat: add required validator

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,60 @@
+function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value
+    const newDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: true,
+        get() {
+            console.log(this);
+            const boundFn = originalMethod.bind(this)
+            return boundFn
+        }
+    }
+    return newDescriptor
+}
+
+interface ValidatorConfig {
+    [property: string]: {
+        [property: string]: string[]
+    }
+}
+
+const vConfig: ValidatorConfig = {}
+
+function Required(target: any, propName: string) {
+   vConfig[target.constructor.name] = {
+       ...vConfig[target.constructor.name],
+       [propName]: [...(vConfig[target.constructor.name]?.[propName] ?? []), 'required']
+   }
+}
+
+function validate(obj: any): boolean {
+    const validators = vConfig[obj.constructor.name]
+    if (!validators) return true
+    let isValid = true
+    for (const prop in validators) {
+        for (const validator of validators[prop]) {
+            switch(validator) {
+                case 'required':
+                    isValid = isValid && !!obj[prop].value
+                    break
+            }
+        }
+    }
+    return isValid
+}
+
 class ProjectInput {
     templateFormEl: HTMLTemplateElement
     appContainer: HTMLDivElement
     formElement: HTMLFormElement
+
+    @Required
     titleInput: HTMLInputElement
+
+    @Required
     descriptionTextArea: HTMLTextAreaElement
+
+    @Required
     numberInput: HTMLInputElement
 
 
@@ -28,10 +79,24 @@ class ProjectInput {
         this.formElement.addEventListener('submit', this.submitHandler)
     }
 
+    private fetchUserInput(): [string, string, number] | undefined {
+        const title = this.titleInput.value
+        const desc = this.descriptionTextArea.value
+        const people = this.numberInput.value
+        if (!validate(this)) {
+            alert('Invalid input, please try again!');
+            return
+        }
+        return [title, desc, +people];
+    }
+    
+    @AutoBind
     private submitHandler(event: Event) {
         event.preventDefault()
-        console.log(this.titleInput.value);
-        
+        const userInput = this.fetchUserInput()
+        if (!Array.isArray(userInput)) return
+        const [title, description, people] = userInput
+        console.log(title, description, people);
     }
 
     private attach() {
